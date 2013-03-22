3dde12a2afd5aa0196ab484f093c189c72f657ce
Alexander Senko <Alexander.Senko@gmail.com>
2013-03-22 17:58:20 +0700

    Development: configured generators.

--- lib/media_store/engine.rb
+++ lib/media_store/engine.rb
@@ -2,6 +2,14 @@ module MediaStore
 	class Engine < ::Rails::Engine
 		isolate_namespace MediaStore
 
+		config.generators do |g|
+			g.javascript_engine :coffee
+			g.stylesheet_engine :sass
+			g.template_engine   :haml
+			g.test_framework    nil
+		end
+
+
 		def self.mounted_at
 			Rails.application.routes.routes.find do |route|
 				route.app == self
