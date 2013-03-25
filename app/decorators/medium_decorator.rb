207d1f3d354a12cc593203dc9fa6b951442e73d1
Alexander Senko <Alexander.Senko@gmail.com>
2013-03-26 02:45:11 +0700

    API: added descriptions to models on display.

--- app/decorators/medium_decorator.rb
+++ app/decorators/medium_decorator.rb
@@ -7,4 +7,19 @@ class MediumDecorator < MediaStore::ModelDecorator
 	alias_method :title,    :name
 	alias_method :headline, :name
 	alias_method :caption,  :name
+
+	def as_json options = {}
+		model.as_json({
+			except: [
+				:filename,
+				:metadata,
+			],
+
+			include: [
+				*I18n.available_locales.map { |lang|
+					:"description_#{lang}"
+				}
+			],
+		}.merge options)
+	end
 end
