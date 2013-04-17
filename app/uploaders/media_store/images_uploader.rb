4fe08953afe58506c5d9c3152f085f569a3cb438
Alexander Senko <Alexander.Senko@gmail.com>
2013-04-18 01:28:51 +0700

    Upload: auto-orient images.

--- app/uploaders/media_store/images_uploader.rb
+++ app/uploaders/media_store/images_uploader.rb
@@ -2,6 +2,8 @@ module MediaStore
 	class ImagesUploader < Uploader
 		# TODO: make DB-driven
 
+		process :auto_orient
+
 		version :thumbnail do
 			process :resize_to_fit => [ 150, 150 ]
 		end
@@ -9,6 +11,14 @@ module MediaStore
 		version :preview do
 			process :resize_to_fit => [ 600, 600 ]
 			process :set_watermark
+		end
+
+		protected
+
+		def auto_orient
+			manipulate! do |image, index, options|
+				image.auto_orient!
+			end
 		end
 	end
 end
